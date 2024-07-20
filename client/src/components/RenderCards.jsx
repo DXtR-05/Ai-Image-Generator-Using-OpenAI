b90d6628c5f039af8efbc39627ac0e3de40b9c5a
Akash Kumar Laha <akashlaha1705@gmail.com>
2024-07-20 18:41:03 +0530

    first commit

---
+++ client/src/components/RenderCards.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./Card";
+
+const RenderCards = ({ data, title }) => {
+  if (data?.length > 0) {
+    return data.map((post) => <Card key={post._id} {...post} />);
+  }
+
+  return (
+    <h2 className="mt-5 font-bold text-[#6469ff] text-xl uppercase">{title}</h2>
+  );
+};
+
+export default RenderCards;
---
+++ server/routes/dalleRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import OpenAI from 'openai';
+
+dotenv.config();
+
+const router = express.Router();
+
+const openai = new OpenAI({
+     apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+});
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL-E!" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const aiResponse = await openai.createImage({
+      prompt,
+      n: 1,
+      size: "1024x1024",
+      response_format: "b64_json",
+    });
+
+    const image = aiResponse.data.data[0].b64_json;
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
+  }
+});
+
+export default router;
